f70db461d17b3dbd27d8a946671c66264fd66125
Splitwirez <Splitwirez@gmail.com>
2019-08-06 14:38:00 -0400

    Why is this slower?

--- src/sporemodder/file/spui/components/DirectImage.java
+++ src/sporemodder/file/spui/components/DirectImage.java
@@ -1,22 +1,24 @@
 /****************************************************************************
-* Copyright (C) 2019 Eric Mor
-*
-* This file is part of SporeModder FX.
-*
-* SporeModder FX is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+ * Copyright (C) 2019 Eric Mor
+ *
+ * This file is part of SporeModder FX.
+ *
+ * SporeModder FX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 package sporemodder.file.spui.components;
+
+import java.nio.IntBuffer;
 
 import javafx.scene.Node;
 import javafx.scene.canvas.GraphicsContext;
@@ -29,6 +31,7 @@ import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
+import javafx.scene.image.WritablePixelFormat;
 import javafx.scene.paint.Color;
 import sporemodder.HashManager;
 import sporemodder.file.ResourceKey;
@@ -40,46 +43,46 @@ import sporemodder.view.inspector.InspectorReferenceLink;
 import sporemodder.view.inspector.PropertyPane;
 
 public class DirectImage extends InspectableObject implements ISporeImage {
-	
+
 	private static InspectorReferenceLink referenceLink;
 	private ImageView imageView;
 	private ScrollPane scrollPane;
-	
+
 	private final ResourceKey key;
 	private Image image;
-	
+
 	public DirectImage(Image image, ResourceKey key) {
 		this.image = image;
 		this.key = key;
 	}
-	
+
 	public DirectImage(DirectImage other) {
 		key = new ResourceKey();
 		copy(other);
 	}
-	
+
 	public void copy(DirectImage other) {
 		this.image = other.image;
 		this.key.copy(other.key);
 	}
-	
+
 	@Override public String toString() {
 		//return "Image: " + key.toString();
 		return "Image: " + getLinkString();
 	}
-	
+
 	public ResourceKey getKey() {
 		return key;
 	}
-	
+
 	public void setKey(ResourceKey key) {
 		this.key.copy(key);
 	}
-	
+
 	public Image getImage() {
 		return image;
 	}
-	
+
 	public void setImage(Image image) {
 		this.image = image;
 	}
@@ -104,13 +107,13 @@ public class DirectImage extends InspectableObject implements ISporeImage {
 			hueVal -= 360;
 		while (hueVal < 0)
 			hueVal += 360;
-		
+
 		hueVal += 180;
-		
+
 		tint.setHue(map(hueVal, 0, 360, -1, 1));
 		tint.setSaturation(shadeColor.getSaturation());
 		tint.setBrightness(map(shadeColor.getBrightness(), 0, 1, -1, 0));*/
-		
+
 		if (image == null) {
 			graphics.setFill(shadeColor);
 			graphics.fillRect(dx, dy, dw, dh);
@@ -132,41 +135,41 @@ public class DirectImage extends InspectableObject implements ISporeImage {
 			int sourceY = (int)sy;
 			int sourceRight = (int)sw + sourceX;
 			int sourceBottom = (int)sh + sourceY;
-			
+
 			PixelReader reader = image.getPixelReader();
 			WritableImage wrImage = new WritableImage(imgWidth, imgHeight);
 			PixelWriter writer = wrImage.getPixelWriter();
-			for (int x = 0; x < sourceRight; x++) {
-				for (int y = 0; y < sourceBottom; y++) {
-					//Begin awkward nesting (looks dumb but helps performance)
-					int safeSourceX = sourceX + x;
-					if ((safeSourceX >= 0) && (safeSourceX < imgWidth)) {
-						int safeSourceY = sourceY + y;
-						if ((safeSourceY >= 0) && (safeSourceY < imgHeight)) {
-							int safeDestX = x;
-							if (safeDestX >= 0 && (safeDestX < imgWidth)) {
-								int safeDestY = y;
-								if ((safeDestY >= 0) && (safeDestY < imgHeight)) {
-									//End awkward nesting
-									Color initialColor = reader.getColor(safeSourceX, safeSourceY); //(int)(sx + x), (int)(sx + y));
-									double red = shadeColor.getRed();
-									double green = shadeColor.getGreen();
-									double blue = shadeColor.getBlue();
-									double alpha = shadeColor.getOpacity();
-									writer.setColor(safeSourceX, safeSourceY, new Color(initialColor.getRed() * red,
-											initialColor.getGreen() * green,
-											initialColor.getBlue() * blue,
-											initialColor.getOpacity() * alpha));
-								}
-							}
-						}
-					}
-				}
-			}
+			
+			int[] originalPixels = new int[imgWidth * imgHeight];
+			WritablePixelFormat<IntBuffer> pixelFormat = PixelFormat.getIntArgbPreInstance();
+		    reader.getPixels(0, 0, imgWidth, imgHeight, pixelFormat, originalPixels, 0, imgWidth);
+
+		    float shadeAlpha = (float)(shadeColor.getOpacity());
+		    float shadeRed = (float)(shadeColor.getRed());
+		    float shadeGreen = (float)(shadeColor.getGreen());
+		    float shadeBlue = (float)(shadeColor.getBlue());
+		    int[] tintedPixels = new int[originalPixels.length];
+		    for (int i = 0 ; i < originalPixels.length; i++) {
+		        int c = originalPixels[i];
+		        int alpha = (c >> 24) & 0xFF;
+		        int red = (c >> 16) & 0xFF;
+		        int green = (c >> 8) & 0xFF;
+		        int blue = c & 0xFF;
+
+		        /*WORKS BUT SLOW
+		        java.awt.Color color = new java.awt.Color((int)(red * shadeRed), (int)(green * shadeGreen), (int)(blue * shadeBlue), (int)(alpha * shadeAlpha));
+		        tintedPixels[i] = color.getRGB();
+		        */
+		        
+		        tintedPixels[i] = ((int)(alpha * shadeAlpha) << 24) | ((int)(red * shadeRed) << 16) | ((int)(green * shadeGreen) << 8) | ((int)(blue * shadeBlue) << 0); 
+		    }
+
+		    writer.setPixels(0, 0, imgWidth, imgHeight, pixelFormat, tintedPixels, 0, imgWidth);
+
 			graphics.drawImage(wrImage, sx, sy, sw, sh, dx, dy, dw, dh);
 		}
 	}
-	
+
 	private void setImageView(ImageView imageView, ScrollPane scrollPane) {
 		imageView.setImage(image);
 		if (image != null) {
@@ -177,20 +180,20 @@ public class DirectImage extends InspectableObject implements ISporeImage {
 	@Override
 	public Node generateUI(SpuiEditor editor) {
 		PropertyPane pane = new PropertyPane();
-		
+
 		imageView = new ImageView();
 		scrollPane = new ScrollPane(imageView);
 		scrollPane.setPrefWidth(Double.MAX_VALUE);
 		scrollPane.setMaxHeight(280);
-		
+
 		setImageView(imageView, scrollPane);
-		
+
 		pane.add(PropertyPane.createTitled("Preview", null, scrollPane));
-		
-		
+
+
 		referenceLink = new InspectorReferenceLink();
 		referenceLink.setValue(getLinkString());
-		
+
 		referenceLink.setOnButtonAction(event -> {
 			DirectImage newValue = editor.showImageFileChooser(key);
 			if (newValue != null) {
@@ -198,24 +201,24 @@ public class DirectImage extends InspectableObject implements ISporeImage {
 				setValue(newValue);
 				editor.repaint();
 				editor.refreshTree();
-				
+
 				editor.addEditAction(new SpuiPropertyAction<DirectImage>(oldValue, newValue, 
-					v ->  {
-						setValue(v);
-						editor.repaint();
-						editor.refreshTree();
-					}, 
-					"Image: File"));
+						v ->  {
+							setValue(v);
+							editor.repaint();
+							editor.refreshTree();
+						}, 
+						"Image: File"));
 			}
 		});
-		
+
 		pane.add("File", null, referenceLink.getNode());
 		pane.add("Width", null, new Label(getWidth() + " px"));
 		pane.add("Height", null, new Label(getHeight() + " px"));
-		
+
 		return pane.getNode();
 	}
-	
+
 	private void setValue(DirectImage value) {
 		copy(value);
 		if (imageView != null) setImageView(imageView, scrollPane);

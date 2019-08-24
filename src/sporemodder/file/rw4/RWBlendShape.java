21980d06ead5dbcb7c57a1b95628edec4517f7c3
Emd4600 <emd4600@gmail.com>
2019-08-24 14:13:48 +0200

    Missing files in previous commit.

---
+++ src/sporemodder/file/rw4/RWBlendShape.java
@@ -0,0 +1,64 @@
+package sporemodder.file.rw4;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import emord.filestructures.StreamReader;
+import emord.filestructures.StreamWriter;
+
+public class RWBlendShape extends RWObject {
+
+	public static final int TYPE_CODE = 0xff0002;
+	public static final int ALIGNMENT = 4;
+	
+	public int id;
+	public final List<Integer> shapeIDs = new ArrayList<>();
+	
+	public RWBlendShape(RenderWare renderWare) {
+		super(renderWare);
+	}
+
+	@Override
+	public void read(StreamReader stream) throws IOException {
+		stream.readLEInt();  // gets replaced by code
+		stream.readLEInt();  // Pointer to a function, gets replaced
+		
+		stream.readLEInt();  // Index to subreference in this same object
+		int shapeCount = stream.readLEInt();
+		stream.readLEInt();  // Index to subreference in this same object
+		if (shapeCount != stream.readLEInt()) {
+			throw new IOException("Malformed BlendShape.");
+		}
+		
+		// This is a buffer for the shape times, gets replaced by code
+		stream.skip(4 * shapeCount);
+		
+		for (int i = 0; i < shapeCount; ++i) {
+			shapeIDs.add(stream.readLEInt());
+		}
+	}
+
+	@Override
+	public void write(StreamWriter stream) throws IOException {
+		RWObject placeholder = new RWBlendShape(renderWare);
+		renderWare.addReference(this, 0x1C);
+		renderWare.addReference(placeholder, 0x1C + shapeIDs.size() * 4);
+		
+		stream.writeLEInt(0);
+		stream.writeLEInt(0);
+		stream.writeLEInt(renderWare.indexOf(this, RenderWare.INDEX_SUB_REFERENCE));
+		stream.writeLEInt(shapeIDs.size());
+		stream.writeLEInt(renderWare.indexOf(placeholder, RenderWare.INDEX_SUB_REFERENCE));
+		stream.writeLEInt(shapeIDs.size());
+		stream.writePadding(shapeIDs.size() * 4);
+		for (int i : shapeIDs) stream.writeLEInt(i);
+	}
+
+	@Override
+	public int getTypeCode() {
+		return TYPE_CODE;
+	}
+	
+
+}
---
+++ src/sporemodder/file/rw4/RWBlendShapeBuffer.java
@@ -0,0 +1,97 @@
+package sporemodder.file.rw4;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import emord.filestructures.StreamReader;
+import emord.filestructures.StreamWriter;
+
+public class RWBlendShapeBuffer extends RWObject {
+
+	public static final int TYPE_CODE = 0x200af;
+	public static final int ALIGNMENT = 16;
+	
+	public static final int INDEX_POSITION = 0;
+	public static final int INDEX_NORMAL = 1;
+	public static final int INDEX_TANGENT = 2;
+	public static final int INDEX_TEXCOORD = 3;
+	public static final int INDEX_BLENDINDICES = 9;
+	public static final int INDEX_BLENDWEIGHTS = 10;
+	
+	// position, normal, tangent, texcoord, ?, ?, ?, ?, ?, blendIndices (as shorts), blendWeights (as floats)
+	public final byte[][] data = new byte[11][];
+	public int shapeCount;  //TODO not really! shapeCount - 1?
+	public int vertexCount;
+	
+	public RWBlendShapeBuffer(RenderWare renderWare) {
+		super(renderWare);
+	}
+
+	@Override
+	public void read(StreamReader stream) throws IOException {
+		long baseOffset = stream.getFilePointer();
+		
+		int type = stream.readLEInt();
+		if (type != 1) {
+			// Other type is 0x40
+			throw new IOException("Malfromed RWBlendShapeBuffer");
+		}
+		
+		List<Long> endOffsets = new ArrayList<>();
+		long[] offsets = new long[data.length];
+		for (int i = 0; i < data.length; ++i) {
+			offsets[i] = stream.readLEUInt();
+			if (offsets[i] != 0) endOffsets.add(offsets[i]);
+		}
+		endOffsets.add((long) sectionInfo.size);
+		shapeCount = stream.readLEInt();
+		vertexCount = stream.readLEInt();
+		
+		Iterator<Long> it = endOffsets.iterator();
+		it.next();
+		
+		for (int i = 0; i < data.length; ++i) {
+			if (offsets[i] != 0) {
+				int size = (int)(it.next() - offsets[i]);
+				stream.seek(baseOffset + offsets[i]);
+				
+				data[i] = new byte[size];
+				stream.read(data[i]);
+			}
+		}
+	}
+
+	@Override
+	public void write(StreamWriter stream) throws IOException {
+		long baseOffset = stream.getFilePointer();
+		stream.writeLEInt(1);
+		stream.writePadding(4 * data.length);
+		stream.writeLEInt(shapeCount);
+		stream.writeLEInt(vertexCount);
+		
+		long[] offsets = new long[data.length];
+		
+		for (int i = 0; i < data.length; ++i) {
+			if (data[i] != null) {
+				long offset = stream.getFilePointer();
+				long alignedOffset = (offset + 16-1) & ~(16-1);
+				offsets[i] = alignedOffset - baseOffset;
+				stream.writePadding((int) (alignedOffset - offset));
+				stream.write(data[i]);
+			}
+		}
+		
+		long endOffset = stream.getFilePointer();
+		stream.seek(baseOffset + 4);
+		stream.writeLEUInts(offsets);
+		stream.seek(endOffset);
+	}
+
+	@Override
+	public int getTypeCode() {
+		return TYPE_CODE;
+	}
+
+}

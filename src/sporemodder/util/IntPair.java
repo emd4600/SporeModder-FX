6b65f2337d95e416e55ddcbd102124dc6eb53aab
emd4600 <emd4600@gmail.com>
2020-09-04 21:34:31 +0200

    New names and misc changes

---
+++ src/sporemodder/util/IntPair.java
@@ -0,0 +1,15 @@
+package sporemodder.util;
+
+public class IntPair {
+	public int first;
+	public int second;
+	
+	public IntPair() {
+		
+	}
+	
+	public IntPair(int first, int second) {
+		this.first = first;
+		this.second = second;
+	}
+}

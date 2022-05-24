1efcb2130ad7fbd3d1f488c4907db337b30c6cc5
Splitwirez <Splitwirez@gmail.com>
2022-05-24 17:21:41 -0400

    Add compression options to project settings pane

--- src/sporemodder/view/dialogs/ProjectSettingsUI.java
+++ src/sporemodder/view/dialogs/ProjectSettingsUI.java
@@ -28,8 +28,12 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.DirectoryChooser;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Dialog;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TabPane;
@@ -66,6 +70,10 @@ public class ProjectSettingsUI implements Controller {
 	@FXML private Button findPackPathButton;
 	
 	@FXML private ChoiceBox<PackageSignature> signatureBox;
+	
+	@FXML private CheckBox cbPackageCompression;
+	@FXML private HBox compressionThresholdBox;
+	@FXML private Spinner compressionThresholdSpinner; //TextField compressionThresholdField;
 	
 	private Dialog<ButtonType> dialog;
 	
@@ -224,6 +232,14 @@ public class ProjectSettingsUI implements Controller {
 		
 		project.setPackageSignature(signatureBox.getSelectionModel().getSelectedItem());
 		
+		
+		project.setUseCompression(cbPackageCompression.isSelected());
+		project.setCompressionThreshold((int)(compressionThresholdSpinner.getValueFactory().getValue()));
+		//cbPackageCompression
+		//compressionThresholdBox
+		//compressionThresholdField
+		//compressionThresholdSpinner
+		
 		if (saveSettingsOnExit) {
 			project.saveSettings();
 			
@@ -234,6 +250,55 @@ public class ProjectSettingsUI implements Controller {
 			}
 		}
 	}
+	
+	
+	private static class ByteCountSpinnerValueFactory extends SpinnerValueFactory<Integer> {
+		
+		private int min = 0;
+		private int max = Integer.MAX_VALUE;
+		private int step = 1024;
+		private float stepF = step;
+		private boolean roundOnIncrement = true;
+		
+		public ByteCountSpinnerValueFactory(int initialValue) {
+			
+			valueProperty().addListener((o, oldValue, newValue) -> {
+                // when the value is set, we need to react to ensure it is a
+                // valid value (and if not, blow up appropriately)
+                if (newValue < min) {
+                    setValue(min);
+                } else if (newValue > max) {
+                	setValue(max);
+            	}
+            });
+			
+			setValue(initialValue >= min && initialValue <= max ? initialValue : min);
+		}
+
+		
+		public void setRoundOnIncrement(boolean newValue) {
+			roundOnIncrement = newValue;
+		}
+		
+		
+		private int getRoundedValue() {
+			int value = getValue();
+			return value;
+			//return roundOnIncrement ? (step * Math.round(value / stepF)) : value;
+		}
+		
+		@Override
+		public void decrement(int steps) {
+            final int newIndex = getRoundedValue() - steps * step;
+            setValue(newIndex >= min ? newIndex : min);
+		}
+
+		@Override
+		public void increment(int steps) {
+			final int newIndex = getRoundedValue() + steps * step;
+            setValue(newIndex <= max ? newIndex : max);
+		}
+	};
 	
 	private boolean showInternal(boolean saveSettingsOnExit) {
 		dialog.setTitle("Project Settings (" + project.getName() + ")");
@@ -260,6 +325,86 @@ public class ProjectSettingsUI implements Controller {
 		
 		signatureBox.getSelectionModel().select(project.getPackageSignature());
 		
+		cbPackageCompression.selectedProperty().addListener((obs, oldValue, newValue) -> {
+			compressionThresholdBox.setDisable(!newValue);
+		});
+		cbPackageCompression.setSelected(project.getUseCompression());
+		
+		ByteCountSpinnerValueFactory byteCountFactory = new ByteCountSpinnerValueFactory(project.getCompressionThreshold());
+		compressionThresholdSpinner.setValueFactory(byteCountFactory);
+		/*compressionThresholdSpinner.setOnKeyReleased((value) -> {
+			ensureCompressionThresholdSpinnerValue(byteCountFactory);
+		});*/
+		//boolean refreshValue = true;
+		TextField compressionThresholdSpinnerEditor = compressionThresholdSpinner.getEditor();
+		compressionThresholdSpinnerEditor.textProperty().addListener((obs, oldValue, newValue) -> {	
+			//if (refreshValue) {
+				try {
+					int value = 0;
+					String newValL = newValue.replace(",", "").replace(".", "").replace(" ", "").toLowerCase();
+					if (newValL.endsWith("b"))
+					{
+						int charIndex = -1;
+						for (int i = 0; i < newValL.length(); i++) {
+							if (!Character.isDigit(newValL.charAt(i))) {
+								charIndex = i;
+								break;
+							}
+						}
+						if (charIndex <= -1)
+							value = Integer.parseInt(newValue);
+						else {
+							String newValSuffix = newValL.substring(charIndex).toLowerCase();
+							
+							int mult = 1;
+							int valueRaw = -1;
+							
+							if (newValSuffix.equalsIgnoreCase("kb") || newValSuffix.equalsIgnoreCase("kilobytes"))
+								mult = 1024;
+							else if (newValSuffix.equalsIgnoreCase("mb") || newValSuffix.equalsIgnoreCase("megabytes"))
+								mult = 1024 * 1024;
+							else if (newValSuffix.equalsIgnoreCase("gb") || newValSuffix.equalsIgnoreCase("gigabytes"))
+								mult = 1024 * 1024 * 1024;
+							
+							valueRaw = Integer.parseInt(newValL.substring(0, newValL.length() - newValSuffix.length()));
+							
+							int oldVal = (int)(byteCountFactory.getValue());
+							
+							long newValLong = valueRaw * mult;
+							int newVal = valueRaw * mult;
+							
+							if ((valueRaw != 0) && (newVal != 0))
+								value = newVal;
+							else if (valueRaw == 0)
+								value = 0;
+							else //if (newValLong != newVal) //((oldVal != 0) && ((newVal) == 0))
+								value = oldVal;
+						}
+						
+					}
+					else
+						value = Integer.parseInt(newValue);
+					
+					if ((value != byteCountFactory.getValue()) && (value >= 0))
+						byteCountFactory.setValue(value);
+					else
+						ensureCompressionThresholdSpinnerValue(byteCountFactory);
+				}
+				catch (Exception ex) {
+					//refreshValue = false;
+					//compressionThresholdSpinnerEditor.setText(oldValue);
+					//refreshValue = true;
+					ensureCompressionThresholdSpinnerValue(byteCountFactory);
+					//compressionThresholdSpinner.getValueFactory().setValue(compressionThresholdSpinner.getValue());
+				}
+			//}
+		});
+		/*compressionThresholdSpinner.focusedProperty().addListener((obs, oldValue, newValue) -> {
+			if (!newValue)
+				compressionThresholdSpinner.increment(0);
+				//ensureCompressionThresholdSpinnerValue(byteCountFactory);
+		});*/
+		
 		dialog.setOnShown(event -> {
 			// Make the user start editing the project name
 			tfName.requestFocus();
@@ -276,6 +421,22 @@ public class ProjectSettingsUI implements Controller {
 		else {
 			return false;
 		}
+	}
+	
+	void ensureCompressionThresholdSpinnerValue(ByteCountSpinnerValueFactory byteCountFactory) {
+		System.out.println("VALUE WAS: " + byteCountFactory.getValue());
+		if (byteCountFactory.getValue() > 1024) {
+			byteCountFactory.decrement(0);
+			byteCountFactory.increment(0);
+		}
+		else {
+			byteCountFactory.increment(0);
+			byteCountFactory.decrement(0);
+		}
+		System.out.println("VALUE IS: " + byteCountFactory.getValue());
+		/*byteCountFactory.setRoundOnIncrement(false);
+		compressionThresholdSpinner.increment(0);
+		byteCountFactory.setRoundOnIncrement(true);*/
 	}
 
 	public static boolean show(Project project, boolean saveSettingsOnExit) {
